8386e0a4239322e6b10ef638861375e6eabde7e4
Sara Federico <sara.federico99@hotmail.it>
2023-09-27 13:17:27 +0200

    First commit

---
+++ launch/visualize_wsg32.launch.py
@@ -0,0 +1,37 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    wsg32_xacro_file = os.path.join(get_package_share_directory('wsg32_description'), 'grippers',
+                                     'wsg32.xacro')
+    robot_description = Command(
+        [FindExecutable(name='xacro'), ' ', wsg32_xacro_file])
+
+    rviz_file = os.path.join(get_package_share_directory('wsg32_description'), 'rviz',
+                             'visualize_wsg32.rviz')
+
+    return LaunchDescription([   
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_description}],
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui'
+        ),
+        Node(package='rviz2',
+             executable='rviz2',
+             name='rviz2',
+             arguments=['--display-config', rviz_file]
+             )
+    ])
